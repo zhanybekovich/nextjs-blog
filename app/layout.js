e15330ba7322376ad1f39806caf680e74bd6bf7a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-26 23:04:57 +0600

    Add navbar, footer structure

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>&copy; Blogger. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
