94eab07151b7965aacf83910813215610edebfba
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-29 22:02:25 +0600

    Add footer styles

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.container}>
       <div>&copy; Blogger. All rights reserved.</div>
     </footer>
   );
