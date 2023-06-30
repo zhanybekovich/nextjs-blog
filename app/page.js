1cbfe01be2c87c5bcad71695d8dabbf8431784e3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-30 20:01:56 +0600

    Add hero

--- app/page.js
+++ app/page.js
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Hero from "public/img/hero.jpeg";
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the reams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero image" className={styles.img} />
+      </div>
+    </div>
+  );
 }
