d1b501e9cf9271909116d77df2977c2d4df5593d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-07-01 21:50:25 +0600

    Add portfolio

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div className={styles.container}>Portfolio Page</div>;
+  return <div className={styles.container}>
+    <h1 className={styles.selectTitle}>Choose a gallery</h1>
+    <div className={styles.items}>
+      <Link className={styles.item} href="/portfolio/illustrations">
+        <span className={styles.title}>Illustrations</span>
+      </Link>
+      <Link className={styles.item} href="/portfolio/websites">
+        <span className={styles.title}>Websites</span>
+      </Link>
+      <Link className={styles.item} href="/portfolio/applications">
+        <span className={styles.title}>Applications</span>
+      </Link>
+    </div>
+  </div>;
 };
 
 export default Portfolio;
