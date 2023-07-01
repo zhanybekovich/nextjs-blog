a13cd27607857c1b5db716da490e97aa4007510f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-07-01 22:17:56 +0600

    Add blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog Page</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src="/img/website.jpg"
+            alt="Website"
+            fill={true}
+          />
+        </div>
+        <div className={styles.content}>
+          <h3 className={styles.title}>Title</h3>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src="/img/website.jpg"
+            alt="Website"
+            fill={true}
+          />
+        </div>
+        <div className={styles.content}>
+          <h3 className={styles.title}>Title</h3>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
